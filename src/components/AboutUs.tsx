592adca670513caf31f3528b638b56a6901ae46f
Ahmed Mamdouh <ahmed.mamdouh@coformatique.com>
2023-12-15 22:33:18 +0200

    fix: strechted image

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -41,12 +41,12 @@ const AboutUs = () => {
                 <Typography variant="h1" fontFamily={"initial"} fontWeight={400} color="primary" sx={{ position: 'relative' }}>
                     ABOUT US
                 </Typography>
-                <Typography variant="subtitle2" fontStyle={"italic"} fontSize="1.25rem" sx={{ color: 'white', mt: 2, width: '60%' }}>
+                <Typography variant="subtitle2" fontStyle={"italic"} fontSize="1.25rem" fontWeight={500} sx={{ color: 'white', mt: 2, width: '60%' }}>
                     Amunique is your gateway to the secrets of Ancient Egyptian beauty. Our products and services draw inspiration from the iconic makeup looks, symbols, and traditions of this remarkable culture.
                 </Typography>
             </TextSection>
             <ImageSection>
-                <img src="about-us-image.png" alt="About Us Image" style={{ objectFit: 'fill', width: '100%', height: '100%' }} />
+                <img src="about-us-image.png" alt="About Us Image" style={{ objectFit: 'contain', width: '100%', height: '100%' }} />
             </ImageSection>
             <Box position="absolute" width="200px" height="200px" top={"26%"} left="40%">
                 <img src="background.png" alt="background-2" style={{ objectFit: 'contain', opacity: 0.05 }} />
